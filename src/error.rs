2e1f753d23053f7cd8056573ed0c60b066c8e12a
Jesper Dalberg <jdalberg@gmail.com>
2025-06-11 14:32:30 +0200

    Error enum for the USP component

---
+++ src/error.rs
@@ -0,0 +1,9 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum UspError {
+    #[error("to_id must be set before building the record")]
+    ToIdNotSet,
+    #[error("Record type must be set before building the record")]
+    RecordTypeNotSet,
+}
