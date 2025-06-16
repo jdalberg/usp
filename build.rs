40c2cbdecfc069f81544b621d51c88e91e1a6f59
Jesper Dalberg <jdalberg@gmail.com>
2025-06-16 12:13:31 +0200

    Added serde

--- build.rs
+++ build.rs
@@ -1,7 +1,15 @@
 fn main() {
-    prost_build::compile_protos(&["proto/usp-msg-1-1.proto"], &["proto"])
+    let mut prost_usp_build = prost_build::Config::new();
+    prost_usp_build.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+
+    let mut prost_usp_record_build = prost_build::Config::new();
+    prost_usp_record_build.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+
+    prost_usp_build
+        .compile_protos(&["proto/usp-msg-1-1.proto"], &["proto"])
         .expect("Failed to compile Protobufs for USP messages");
-    prost_build::compile_protos(&["proto/usp-record-1-1.proto"], &["proto"])
+    prost_usp_record_build
+        .compile_protos(&["proto/usp-record-1-1.proto"], &["proto"])
         .expect("Failed to compile Protobufs for USP records");
 }
 // This build script compiles the Protobuf definitions found in `proto/usp-msg.proto`
